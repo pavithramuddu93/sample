0c0fbe25a5127298ba4e9cf2d227dced83037763
pavithramuddu93 <100592978+pavithramuddu93@users.noreply.github.com>
2022-03-03 18:39:33 +0530

    Add files via upload

---
+++ DictionaryExample.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+counter=0
+Fruits[((counter++))]="apple"
+Fruits[((counter++))]="mango"
+Fruits[((counter++))]="orange"
+
+echo ${Fruits[@]}
+echo ${!Fruits[@]}
+echo ${#Fruits[@]}
+echo ${Fruits[counter++]}
+
---
+++ array.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+
+isPartTime=1
+isFullTime=2
+maxRateInMonth=100
+empRatePerHr=20
+numOfWorkingDays=20
+
+totalEmpHr=0
+totalWorkingDays=0
+function getWorkingHours()
+{
+        case $randomCheck in
+                        $isFullTime )
+                                empHrs=8
+                        ;;
+                        $isPartTime )
+                                empHrs=4
+                        ;;
+                        *)
+                                empHrs=0
+                        ;;
+        esac
+}
+function getEmpWage() {
+        echo $(($totalEmpHr*$empRatePerHr))
+}
+
+while [[ $totalEmpHr -lt $maxRateInMonth
+ && $totalWorkindays -lt $numOfWorkingDays ]]
+do
+	((totalWorkingsDays++))
+	randomCheck=$((RANDOM%3))
+	getWorkingHours $randomCheck
+	totalEmpHr=$(($totalEmpHr+$empHrs))
+	dailyWages[$totalWorkingDays]=$(($empHrs*$empRatePerHr))
+done
+totalsalary="$( getEmpWage $totalEmpHr )"
+echo ${dailyWages[@]}
