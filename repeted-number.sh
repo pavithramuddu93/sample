3ad8baf08a145d211fb1632712bc5ccc4d6ff081
pavithramuddu93 <100592978+pavithramuddu93@users.noreply.github.com>
2022-03-03 18:41:48 +0530

    Add files via upload

---
+++ repeted-number.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+function repeatedNumbers(){
+    let arrayOfRepeatedNumbers = [];
+    for(i = 1; i <= 100; i++){
+        if(i % 11 == 0){
+            arrayOfRepeatedNumbers.push(i);
+        }
+    }
+    console.log(`Repeated numbers from 1 to 100 are : ${arrayOfRepeatedNumbers}`);
+}
+repeatedNumbers();
